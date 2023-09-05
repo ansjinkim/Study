de0b5f8d839f6b5dededceaef14095ab67b96169
Jinhong Kim <jinhong.kim@ansys.com>
2023-09-05 11:34:53 +0900

    aa

--- scripts/create_conan.ps1
+++ scripts/create_conan.ps1
@@ -1,8 +1,8 @@
 Expand-Archive -Path Post.zip -DestinationPath Post
 
-New-Item -Path ansys.motion.post_api
-New-Item -Path ansys.motion.post_api\Resources
-New-Item -Path ansys.motion.post_api\x64
+New-Item -ItemType Directory -Path ansys.motion.post_api
+New-Item -ItemType Directory -Path ansys.motion.post_api\Resources
+New-Item -ItemType Directory -Path ansys.motion.post_api\x64
 
 Copy-Item -Path Post\Resources\DataSource.db -Destination ansys.motion.post_api\Resources
 
